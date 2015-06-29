674f1efeebd596409cef7457890e66945ce1d474
nicholasceliano <nicholasceliano@yahoo.com>
2015-06-29 10:49:24 -0400

    Migration off of TFS

---
+++ Website/WebSite/Controllers/AJCController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class AJCController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Website/WebSite/Controllers/WorkController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class WorkController : Controller
+    {
+        // GET: Work
+        public ActionResult CleanRooms()
+        {
+            return View();
+        }
+       
+        public ActionResult RetroFit()
+        {
+            return View();
+        }
+
+        public ActionResult WorkEthic()
+        {
+            return View();
+        }
+
+        public ActionResult WorkLib()
+        {
+            return View();
+        }
+    }
+}
