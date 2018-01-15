a0353623660dfa95bce6502da4f1777ceefd3591
Nicholas Celiano <nicholasceliano@gmail.com>
2018-01-15 16:23:11 -0700

    Update

--- Website/WebSite/Global.asax.cs
+++ Website/WebSite/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -14,6 +10,7 @@ namespace WebSite
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
