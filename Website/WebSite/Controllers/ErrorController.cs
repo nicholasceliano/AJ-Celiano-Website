cc18c8df696cb8e878976f4e78c24aa1e8f195a0
Nicholas Celiano <nicholasceliano@gmail.com>
2018-04-17 19:43:16 -0400

    Final changes

---
+++ Website/WebSite/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+    }
+}
